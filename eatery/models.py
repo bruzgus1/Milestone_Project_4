0812d9b4a0a36a129b6e69cc29effec20dc71ac4
bruzgus1 <poviliukas987412365@gmail.com>
2022-06-10 10:41:25 +0000

    created database model

--- eatery/models.py
+++ eatery/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Reservation(models.Model):
+
+    first_name = models.CharField(max_length=200, unique=True)
+    last_name = models.CharField(max_length=200, unique=True)
+    date = models.DateTimeField()
+    number_of_guests = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name
