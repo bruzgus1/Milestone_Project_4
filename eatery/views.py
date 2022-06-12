6b1b54dcddf6a044f4c41226a0e8a2c5db8586f3
bruzgus1 <poviliukas987412365@gmail.com>
2022-06-12 13:53:32 +0000

    loaded up my index.html template

--- eatery/views.py
+++ eatery/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views import generic
+from django.views.generic import TemplateView
 
-# Create your views here.
+def get_home_page(request):
+    return render(request, 'index.html')
