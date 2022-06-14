d40872514bbafc3fb16437bf883087ee9101e296
bruzgus1 <poviliukas987412365@gmail.com>
2022-06-14 14:00:29 +0000

    installed allauth and updated my settings.py file

--- snack/settings.py
+++ snack/settings.py
@@ -42,11 +42,20 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'eatery',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
