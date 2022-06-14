30c2ab1daa5dfa961187a13f6ad119b0e1d1cdf2
bruzgus1 <poviliukas987412365@gmail.com>
2022-06-14 12:25:03 +0000

    created make_a_reversation.html template and url path to the template

--- eatery/urls.py
+++ eatery/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.get_home_page, name='home')
+    path('', views.get_home_page, name='home'),
+    path('make-reservation', views.get_reversation_page, name='reservation')
 ]
--- eatery/views.py
+++ eatery/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.views import generic
 from django.views.generic import TemplateView
 
+
 def get_home_page(request):
     return render(request, 'index.html')
+
+
+def get_reversation_page(request):
+    return render(request, 'make_a_reservation.html')
