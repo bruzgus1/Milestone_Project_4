5bdb2f51c2016cf0c30ce740d25cacd801d9503d
bruzgus1 <poviliukas987412365@gmail.com>
2022-06-15 17:36:09 +0000

    added a form in make_a_reservation.html template to insert data into model

--- eatery/views.py
+++ eatery/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from django.views.generic import TemplateView
 from .models import Reservation
